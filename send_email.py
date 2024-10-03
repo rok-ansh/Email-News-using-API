9fa405beca2f684260686d6b6b1d84f13eb62197
rohit <rohit26av@gmail.com>
2024-10-03 07:04:43 +0530

    Send email with title and description

---
+++ send_email.py
@@ -0,0 +1,16 @@
+import smtplib, ssl
+
+
+def get_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "rohitdswork1@gmail.com"
+    password = "bitd uvlf mahs roii"
+
+    receiver = "rohitdswork1@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
