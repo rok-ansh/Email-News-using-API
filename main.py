a01adcacc46b0e1d7770d08f7c5e7abab5dd2edc
rohit <rohit26av@gmail.com>
2024-10-03 07:28:39 +0530

    Add Links, specify language to english only, limit  number of news, add dynamic subject, control the category for which type of new we want

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import requests
 from send_email import get_email
 
 api_key = "178dba6ceaf346018f4c7280d95f99e4"
-url = ("https://newsapi.org/v2/everything?q=tesla&"
+topic = "amazon"
+url = ("https://newsapi.org/v2/everything?"
+       f"q={topic}&"
        "from=2024-09-03&sortBy=publishedAt&apiKey"
-       "=178dba6ceaf346018f4c7280d95f99e4")
+       "=178dba6ceaf346018f4c7280d95f99e4&language=en")
 
 # Make a request
 request = requests.get(url)
@@ -15,10 +17,17 @@ content = request.json()
 # Access the article titles and description
 body = ""
 # print(type(body))
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article["description"] is not None:
-        body = body + article["title"] + "\n" + article["description"] + "\n" + 100*"-" + 2*"\n"
+        body = (f"Subject : Today's {topic.title()} News" + "\n" + body + article["title"] + "\n" +
+                article["description"] + "\n" +
+                article["url"] + "\n" +
+                100*"-" + 2*"\n")
 
 body = body.encode("utf-8")
+# message = f"""\
+# Subject : Today's {topic.title()} News
+# {body}
+# """
 get_email(body)
 print("Mail Sent")
